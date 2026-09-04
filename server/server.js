71d265e2719d9dfac265da8f1cc3681a1e713993
Sayan Saha <sayansaha555555@gmail.com>
2026-09-04 12:04:13 +0530

    initial setup

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Create an instance of the Express application
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware 
+app.use(cors());
+app.use(express.json());
+
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
